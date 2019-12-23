fa8e609db3f47e6069467bef1553b9f6f551d93c
JKorum <constantine.proshin@gmail.com>
2019-12-23 19:20:19 +0300

    fix3

---
+++ client/build/precache-manifest.f6b1fbb08c465ee1ecf97d7489cb9863.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a596851f9d133b82d439fba8670b085",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa42d8f8cab7c3e6541",
+    "url": "/static/css/main.5f78c2e0.chunk.css"
+  },
+  {
+    "revision": "7ec705fe43b1727c88b1",
+    "url": "/static/js/2.e5240349.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.e5240349.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2aa42d8f8cab7c3e6541",
+    "url": "/static/js/main.88a13eea.chunk.js"
+  },
+  {
+    "revision": "9dd6ed2f41b916ab9cc9",
+    "url": "/static/js/runtime-main.064d9831.js"
+  },
+  {
+    "revision": "c0f6b244a8fb1dafcc085859a613dc86",
+    "url": "/static/media/berries.c0f6b244.png"
+  }
+]);
